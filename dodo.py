fe252305130654604b3c662c9ab47f802119e844
Westerbly Snaydley <westerbly@gmail.com>
2021-11-03 20:18:50 -0500

    formatting

--- dodo.py
+++ dodo.py
@@ -2,9 +2,9 @@ import platform
 from doit.tools import LongRunning
 from doit.tools import run_once
 
-OS = platform.system()
-is_windows = OS.startswith("MINGW")
-is_linux = OS == "Linux"
+operating_system = platform.system()
+is_windows = operating_system.startswith("MINGW")
+is_linux = operating_system == "Linux"
 
 def task_bin():
     """build the game binary"""
@@ -18,7 +18,7 @@ def task_launch():
     """launch the game"""
     cmd = "./bin/game"
     return {
-            'actions': [LongRunning(cmd)],
-            'file_dep': ["./bin/game"],
-            'uptodate': [False]
-        }
+        'actions': [LongRunning(cmd)],
+        'file_dep': ["./bin/game"],
+        'uptodate': [False]
+    }
